16c14ebfa32adff69f208a42190eda064e0009d4
dbsigurd <dbsigurd@ualberta.ca>
2016-01-30 14:42:06 -0700

    working on model test

---
+++ app/src/test/java/ca/knoblauch/dialog/modelTest.java
@@ -0,0 +1,23 @@
+package ca.knoblauch.dialog;
+
+import org.junit.Test;
+import ca.knoblauch.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+import org.junit.runner.RunWith;
+//import android.support.test.runner.AndroidJUnit4;
+//
+//@RunWith(AndroidJUnit4.class)
+public class modelTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    SpeechModel sm = new SpeechModel();
+
+
+}
