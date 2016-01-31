f903be84a2257211b51ab1ec82772b7129477559
Greg Knoblauch <gknoblau@ualberta.ca>
2016-01-30 22:31:20 -0700

    FUCK EM

--- app/src/main/java/ca/knoblauch/dialog/results.java
+++ app/src/main/java/ca/knoblauch/dialog/results.java
@@ -2,11 +2,16 @@ package ca.knoblauch.dialog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  * Created by gknoblau on 2016-01-30.
  */
 public class results extends Activity{
+    private Button pacing;
+    private Button flaggedWords;
+    private Button readingLevel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
